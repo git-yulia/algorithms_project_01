4b6dc8fbc79469bc0b03c3d461871984d0b6d5d8
Yulia <julia.maliauka953@myci.csuci.edu>
2018-09-18 16:12:36 -0700

    Fixed the small issue where the entire program didn't work and spat out gibberish and ate my memory.

--- assignment_1_euclids.py
+++ assignment_1_euclids.py
@@ -8,34 +8,52 @@ import sys
 m = 0
 n = 0
 
+# This step acquires inputs while verifying that the algorithm's PRE-CONDITIONS are being met. 
 while (m <= 0): m = int(input("Please input an integer greater than 0: "))
 while (n <= 0): n = int(input("And just one more, please: "))
 
 def get_euclids_extended(m, n): 
-    a = 0
-    b = 1
-    x = 1
-    y = 0
-    c = m 
-    d = n 
+    a = 0  
+    x = 1  
+    b = 1  
+    y = 0  
+    c = m   
+    d = n    
+
+    counter = 1 
 
     while(True):
-        q = c / d
+        print("phase ", counter)
+        q = int(c / d)   
+        print("q = ", q)
         r = c % d
+        print("r = ", r)
 
         if (r == 0):
-            return(a,b)
+            return (int(a),int(b))
 
         c = d
+        print("c set to ", c)
         d = r
+        print("d set to ", d)
         t = x
+        print("t set to ", t)
         x = a
-        a = t - (q * a)
+        print("x set to ", x)
+        a = (t - (q * a))
+        print("a set to ", a)
         t = y
+        print("t set to ", t)
         y = b
-        b = t - (q * b)
+        print("y set to ", y)
+        b = (t - (q * b))
+        print("b set to ", b)
+
+        counter = counter + 1
+        print("\n")
 
 print(get_euclids_extended(m,n))
 
-# Make sure that the post-condition requirements were met 
-
+# Make sure that the POST-CONDITION requirements were met 
+# Euclid's extended algorith, given integers m and n, should return (a,b): 
+#       a*m + b*n = gcd(m, n)
