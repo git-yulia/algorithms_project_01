0627e555e1f01c5d7fae0940cf881da80befd625
Yulia <julia.maliauka953@myci.csuci.edu>
2018-10-09 14:10:08 -0700

    added skeleton data structures

--- assignment_2_prims.py
+++ assignment_2_prims.py
@@ -8,15 +8,34 @@
 # At each stage, it adds a new branch to the already-constructed tree T. 
 # The algorithm stops when all nodes have been reached. 
 
-# Initialize T, an empty tree that will later become our MCST. 
+# Initialize our graph 
+# CURRENTLY USING EXAMPLE GRAPH 1 - see the .png in Git repo 
+graph = { "e1" : ["v1", "v2"],  
+          "e2" : ["v1", "v3"],  
+          "e3" : ["v3", "v4"],  
+          "e4" : ["v2", "v4"],  
+          "e5" : ["v2", "v5"],
+          "e6" : ["v4", "v5"]
+        } 
 
 # Initialize V using all the vertices from the graph. 
+# (vertices are all currently set to a cost of 1.)
+vertices = ["v1","v2","v3","v4","v5"]
+edges    = ["e1", "e2", "e3", "e4", "e5", "e6"]  
+
+# Initialize T, an empty tree that will later become our MCST. 
+tree = []
 
 # Initialize B using v, an arbitrary vertex of V (all the vertices). 
+B = ["v1"]
 
 # While B != V: 
+index = 1
+while len(B) < len(vertices): 
+    B.append(vertices[index])
+    print(B)
+    index = index + 1
 
 # find cheapest e = (v1, v2) such that v1 in B, v2 in V - B
 
 # T = T union {e}
-# B = B union {v2}
