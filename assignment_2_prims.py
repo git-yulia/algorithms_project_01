500ed1a965562febb2f2ff34c33c56989eab9377
Yulia <julia.maliauka953@myci.csuci.edu>
2018-10-14 12:22:00 -0700

    Added vertex class and have decreased yelling at computer by 5%, however, I am on the search for bagels at this moment

--- assignment_2_prims.py
+++ assignment_2_prims.py
@@ -5,6 +5,7 @@
 
 import sys 
 import random 
+import math 
 
 # Prim's algorithm for constructing MCSTs grows trees in a natural way, 
 # starting from an arbitrary root. 
@@ -22,18 +23,59 @@ graph = [ [0,2,3,0,0],
 # Initialize V using all the vertices from the graph. 
 vertices = ["v1","v2","v3","v4","v5"]
 
+class vertex:
+    'Contains vertex and information about cheapest edges connected to it'
+
+    def __init__(self, vertex_number, cheapest_edge_cost):
+       self.vertex_number = vertex_number
+       self.name = vertices[vertex_number]
+       self.cheapest_edge_cost = cheapest_edge_cost
+       
+    def get_name(self):
+        return self.name
+
+    def get_key(self):
+        return self.cheapest_edge_cost
+
+    def find_cheapest_edge(self):
+        return 0 
+
+
+
 # Initialize T, an empty tree that will later become our MCST. 
 tree = []
 
-# Initialize B using v, an arbitrary vertex of V (all the vertices). 
-# B is a min-priority queue B
-# keeps track of all the vertices NOT in the tree T 
-# uses: min weight of any edge connecting vertex to t 
-# infinity if no edge exists 
-B = [] 
-
 # pick the arbitrary root to use as our starting point 
-starting_node = random.randint(0, (len(vertices) - 1))
+# starting_node = random.randint(0, (len(vertices) - 1))
+starting_node = 0 
 print("Starting Prim's algorithm at arbitrary root", vertices[starting_node])
 
-# while B is not empty:
+# pseudocode:
+#____________________________________________________________________
+
+# create an array parents[] of size V and fill it with NULL
+parents = [None for index in range(len(vertices))]
+
+# create a min heap of size V. let the min heap be B[]
+B = [None for index in range(len(vertices))]
+
+for new_vertex in range(len(B)):
+    B[new_vertex] = vertex(new_vertex, math.inf) 
+    print("added", B[new_vertex].name)
+
+
+
+# while B is not empty: 
+while(len(B) != 0):
+
+#   a) u (a vertex) = get min(B)
+    u = B.pop()
+    print("(",u.name,",", u.cheapest_edge_cost,")")
+
+#   b) for every adjacent v of u:
+#           if v is in B:
+#               update key value of v in B if weight of edge (u,v) is
+#               smaller than current key value of v 
+#               parent[v] = u 
+
+
