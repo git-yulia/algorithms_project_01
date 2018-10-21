141dfcad6e0f270d92d8094160a6901e2ff7e102
Yulia <julia.maliauka953@myci.csuci.edu>
2018-10-21 12:33:22 -0700

    what is going on lol

--- assignment_2_prims.py
+++ assignment_2_prims.py
@@ -24,9 +24,21 @@ graph = [
     [0, 8, 0, 2, 0],
 ]
 
+edge_names = [
+    [0, "e1", "e2", 0, 0],
+    ["e1", 0, 0, "e4", "e5"],
+    ["e2", 0, 0, "e3", 0],
+    [0, "e4", "e3", 0, "e6"],
+    [0, "e5", 0, "e6", 0],
+]
+
+
 # Initialize V using all the vertices from the graph.
 vertices = ["v1", "v2", "v3", "v4", "v5"]
+edges    = ["e1", "e2", "e3", "e4", "e5", "e6", "e7"]
 
+# by definition of MCST: 
+required_number_of_edges = len(vertices) - 1
 
 class vertex:
     "Contains vertex and information about cheapest edges connected to it"
@@ -58,31 +70,37 @@ class vertex:
 
         return adjacent_vertices
 
-    def find_cheapest_edge(self):
-        self.key = 13
-        return self.key
+    def get_adjacent_edges(self):
+        adjacent_edges = []
+
+        for index in range(len(vertices)):
+            if graph[self.vertex_number][index] != 0:
+                added_edge = edge(self.vertex_number, index, (graph[self.vertex_number][index]))
+                #print("Added an edge to adjacents:", edge_names[added_edge.contents[0]][added_edge.contents[1]])
+                adjacent_edges.append(added_edge)
+
+        return adjacent_edges
 
 class edge:
     def __init__(self, vertex_1, vertex_2, edge_weight):
+        self.name = edge_names[vertex_1][vertex_2]
         self.contents = (vertex_1, vertex_2)
         self.edge_weight = edge_weight
 
 class min_priority_queue:
     "Contains information and methods for dealing with B, our heap structure"
-
     def __init__(self, size_of_heap):
         self.size = size_of_heap
-        self.contents = [vertex(index) for index in range(self.size)]
+        self.contents = [] * required_number_of_edges
 
-    "pop: removes front element at index 0 which is lowest cost, highest priority"
-
+    "pop: removes front edge at index 0 which is lowest cost, highest priority"
     def get_minimum(self):
         popped_item = self.contents[0]
         self.contents.remove(popped_item)
         self.size = self.size - 1
         return popped_item
 
-    "push: appends elements to B and then sorts them" 
+    "push: appends edges to B and then sorts all of them" 
     def push(self, edge):
         pass
 
@@ -90,121 +108,85 @@ class min_priority_queue:
         print("B before sorting:", B)
         print("B after sorting:", B)
 
-    "contains: checks B for existence of desired vertex"
-
-    def contains(self, vertex):
-        found = False
-
-        index = 0
-        while (found == False) and (index < self.size):
-            if self.contents[index].name == vertices[vertex]:
-                found = True
-            index = index + 1
-
-        return found
-
-    def update_key(self, vertex_number, new_key_value):
-        print("Trying to set key for ", vertex_number, "to", new_key_value)
-        self.contents[vertex_number].set_key(new_key_value)
-
-
 # Initialize T, an empty tree that will later become our MCST.
-T = []
 
-# pick the arbitrary root to use as our starting point
-# starting_node = random.randint(0, (len(vertices) - 1))
-starting_node = 0
-print("Starting Prim's algorithm at arbitrary root", vertices[starting_node])
+
+class MCST: 
+    def __init__(self):
+        self.contents = [] 
+        self.size = 0 
+
+    def add_edge(self, edge):
+        self.contents.append(edge)
+        self.size = self.size + 1
+
+    def does_not_contain(self, edge_name):
+        contains_edge = False
+
+        for index in range(len(self.contents)):
+            print(self.contents[index].name)
+            print(edge_name)
+
+            if self.contents[index].name == edge_name:
+                contains_edge = True
+
+        return contains_edge
+
+    def print_contents(self):
+        for edge in self.contents:
+            print(edge.name)
+
+edge_example = edge(0,1,2)
+T = MCST()
+
 
 # create a min heap of size V. let the min heap be B[]
 B = min_priority_queue(len(vertices))
 
-# make starting vertex's key 0 for now
-B.contents[0].set_key(0)
+T.add_edge(edge(0,1,2))
 
-"""
-iterations = 0
-while B.size != 0:
-
-    #_____________________________________________________________________________
-    iterations = iterations + 1
-    print("After", iterations, "iterations, here's what we have: \n")
-    print("B: ", )
-
-    for index in range(len(B.contents)):
-        print("[", index, "]: ", "(", B.contents[index].name, B.contents[index].get_key(), ")")
-
-    print("\nT: ")
-    for index in range(len(T)):
-        if (T[index] != INFINITY): print("[", index, "]: ", vertices[T[index]])
-
-    #_____________________________________________________________________________
-
-    u = B.get_minimum()
-
-    adjacent_vertices = u.get_adjacent_vertices()
-
-    for index in range(len(adjacent_vertices)):
-        if B.contains(adjacent_vertices[index]):
-
-            edge_cost = graph[u.vertex_number][adjacent_vertices[index]]
-
-            print(">>>his edge cost for (",(u.vertex_number),adjacent_vertices[index],") was", edge_cost)
-
-            # update key value of v in B if weight of edge (u,v) is
-            # smaller than current key value of v
-            if edge_cost < u.key and edge_cost != 0:
-                B.update_key(u.vertex_number, edge_cost)
-
-                # sort when you change a key 
-
-
-                print("updated key for ", vertices[u.vertex_number] ," to", B.contents[u.vertex_number].get_key())
-
-    """
-stage = 0
-while B.size != 0:
-    print("STAGE", stage)
-    # set current vertex 
-
-    # push all edges adjacent to our popped dude, SORT 
+def main():
+    stage = 0
+    current_vertex = vertex(0)
     
+    print("Starting Prim's algorithm at arbitrary root", vertices[current_vertex.vertex_number])
 
+    while T.size < required_number_of_edges and stage < 5:
 
-    
-    # current vertex = detination of edge we traveled on (added to T)
+        # Find adjacent_vertices 
+        adjacent_vertices = current_vertex.get_adjacent_edges() 
+        
+        #  push adjacent edges of current_vertex to B and SORT 
+        index = 0 
+        for edge in adjacent_vertices:
+            print(edge.name)
 
+            # only add the edge to B if it is not already in our MCST
+            if T.does_not_contain(edge.name):
+                print("gonna add this edge")    
 
-
-
-
-
-    stage = stage + 1
-
-print(".....................................")
+            index = index + 1
+        #  do not add edge if in T
         
 
+        #  if size(B) == 0 (no edges), current_vertex = find_vertex_with_new_paths(), push those to B and SORT 
 
+        #  pop top edge (cheapest) of B to T if T does not already contain e(i)
 
+        #  set new current_vertex to T[]'s last element's tail 
 
-"""
-5) While either pq doesn't become empty 
-    a) Extract minimum key vertex from pq. 
-       Let the extracted vertex be u.
+        
 
-    b) Include u in MST using inMST[u] = true.
+        
 
-    c) Loop through all adjacent of u and do 
-       following for every vertex v.
+        print("[ STAGE", stage, "]")
+        print("B = ", B.contents)
+        print("T = ", T.print_contents())
+        print("current_vertex    = ", vertices[current_vertex.vertex_number])
+        print("_________________________________________")
+        stage = stage + 1
 
-           // If weight of edge (u,v) is smaller than
-           // key of v and v is not already in MST
-           If inMST[v] = false && key[v] > weight(u, v)
+    print("T = ", T.print_contents())
+        
 
-               (i) Update key of v, i.e., do
-                     key[v] = weight(u, v)
-               (ii) Insert v into the pq 
-               (iv) parent[v] = u
-               
-6) Print MST edges using parent array.
-"""
+main() 
