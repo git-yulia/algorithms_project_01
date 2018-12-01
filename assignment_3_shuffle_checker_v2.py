b9db528b8b417f38dad684fbe291f2ce1d0b1cfa
git-yulia <julia.maliauka953@myci.csuci.edu>
2018-11-30 20:18:04 -0800

    Changed a lot of things...seems to work for most test cases, but some strange ones break it..

--- assignment_3_shuffle_checker_v2.py
+++ assignment_3_shuffle_checker_v2.py
@@ -15,33 +15,40 @@ try:
 except SyntaxError:
     W = ""
 
+
 def check_shuffle(U, V, W):
     if len(W) != (len(U) + len(V)):
-        print(W, "is not an interleaving of", U, "and", V, ".")
+        print(W, "is not an interleaving of", U, "and", V)
         return False
 
-    graph = [[False for x in range(0, len(U) + 2)] for y in range(0, len(V) + 2)]
+    graph = [[0 for x in range(0, len(U) + 1)] for y in range(0, len(V) + 1)]
+
+    # The graph at (0,0) is true regardless of circumstance
     graph[0][0] = True
 
     # the edges along the 'outside' of the graph have one parent each, so set them specially
-    for i in range(1, len(V)):
-        graph[0][i] = (graph[0][i - 1] and V[i] == W[i])
-    for j in range(1, len(U)):
-        graph[j][0] = (graph[j - 1][0] and U[j] == W[j])
-
     for i in range(0, len(U)):
         for j in range(0, len(V)):
-            print("i: ", i, " j: ", j)
-            graph[i][j] = (graph[i - 1][j] and U[i] == W[i + j]) or (
-                graph[i][j - 1] and V[j] == W[i + j]
-            )
-            print("g[i][j]: ", graph[i][j])
+            # print("i: ", i, " j: ", j)
+            if i == 0 and j == 0:
+                graph[i][j] = True
+            elif i == 0 and V[j - 1] == W[j - 1]:
+                graph[i][j] = graph[i][j - 1]
+            elif (U[i - 1] == W[i + j - 1]) and (V[j - 1] != W[i + j - 1]):
+                graph[i][j] = graph[i - 1][j]
+            elif (U[i - 1] != W[i + j - 1]) and (V[j - 1] == W[i + j - 1]):
+                graph[i][j] = graph[i][j - 1]
+            elif (U[i - 1] == W[i + j - 1]) and (V[j - 1] == W[i + j - 1]):
+                graph[i][j] = graph[i - 1][j] or graph[i][j - 1]
+            else:
+                graph[i][j] = False
 
     if graph[len(U) - 1][len(V) - 1] == True:
-        print(W, "is an interleaving of ", U, " and ", V, ".")
+        print("\n", W, "is an interleaving of ", U, " and ", V)
         return True
     else:
-        print(W, "is not an interleaving of ", U, " and ", V, ".")
-        return False 
+        print("\n", W, "is not an interleaving of ", U, " and ", V)
+        return False
+
 
 check_shuffle(U, V, W)
