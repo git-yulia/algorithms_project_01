abfc84474efc8f9bb40b027ccfe13b23e18134ec
git-yulia <julia.maliauka953@myci.csuci.edu>
2018-11-30 22:40:40 -0800

    flipped U and V in the graph creation part since that was messing the indexing up

--- assignment_3_shuffle_checker_v2.py
+++ assignment_3_shuffle_checker_v2.py
@@ -18,22 +18,23 @@ except SyntaxError:
 
 def check_shuffle(U, V, W):
     if len(W) != (len(U) + len(V)):
-        print(W, "is not an interleaving of", U, "and", V)
+        print("No")
         return False
 
-    graph = [[0 for x in range(0, len(U) + 1)] for y in range(0, len(V) + 1)]
+    graph = [[0 for x in range(0, len(V) + 1)] for y in range(0, len(U) + 1)]
 
     # The graph at (0,0) is true regardless of circumstance
     graph[0][0] = True
 
     # the edges along the 'outside' of the graph have one parent each, so set them specially
-    for i in range(0, len(U)):
-        for j in range(0, len(V)):
-            # print("i: ", i, " j: ", j)
+    for i in range(0, len(U) + 1):
+        for j in range(0, len(V) + 1):
             if i == 0 and j == 0:
                 graph[i][j] = True
             elif i == 0 and V[j - 1] == W[j - 1]:
                 graph[i][j] = graph[i][j - 1]
+            elif j == 0 and U[i - 1] == W[i - 1]:
+                graph[i][j] = graph[i - 1][j]
             elif (U[i - 1] == W[i + j - 1]) and (V[j - 1] != W[i + j - 1]):
                 graph[i][j] = graph[i - 1][j]
             elif (U[i - 1] != W[i + j - 1]) and (V[j - 1] == W[i + j - 1]):
@@ -43,11 +44,11 @@ def check_shuffle(U, V, W):
             else:
                 graph[i][j] = False
 
-    if graph[len(U) - 1][len(V) - 1] == True:
-        print("\n", W, "is an interleaving of ", U, " and ", V)
+    if graph[len(U)][len(V)] == True:
+        print("Yes")
         return True
     else:
-        print("\n", W, "is not an interleaving of ", U, " and ", V)
+        print("No")
         return False
 
 
